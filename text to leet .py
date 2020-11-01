9e4fda0bc2ecc9e34f6daeef7b3db0ec3227751a
Ravi-singh-05 <rvsng0027@gmail.com>
2020-11-01 22:55:11 +0530

    well commented code ready for upload

--- text to leet .py
+++ text to leet .py
@@ -1,5 +1,7 @@
 import random
+
 def leet_list(x):
+    #this 'x' we will use to fetch the correct word and then it will  put a leet word in place of real word
     A = ['4', "/\"", '''@''', 'aye', '(L']
     B = ['I3', '8', '13', '|3', 'ß', '!3', '(3', '/3', ')3', '|-]', 'j3', '6']
     C = ['[', '¢', '{', '<', '(', '©']
@@ -27,25 +29,31 @@ def leet_list(x):
     X = ['><', 'Ж', '}{', 'ecks', ')(', '][']
     Y = ['`/', 'Ч', '7', '\|/', '¥']
     Z = ['2', '7_', '-/_']
+    #creting list of list for further processing..
     leet_li=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
     leet_lit=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     for it in range(65,91):
         zs=x.upper()
+        #zs convert the 'x' in upper case in case if it was not already in upper case
         if zs in leet_lit:
             z=leet_lit.index(zs)
         return leet_li[z] 
 
-
+ 
 con=input("enter the string you want to conver in to leet form-->")
+#con variable mean text you want to convert
 leet_word=''
 wor= list(con)
 for iterable in wor:
     if iterable !=" ":
         num=leet_list(iterable)
-        cal = random.randint(0,len(num)-1)
+        # this variable num will get leet list of the respected variables you will enter
+        cal = random.randint(0,len(num)-1) # this is just to create randomness and also to choose element from list.
         leet_word+=num[cal]
     else:
-        leet_word+=" "
+        leet_word+=" " #This  is because if you enter a <space> between two latters so it will also give space in the leet word
 print()
-print(leet_word)    
-input()    
+print(leet_word)   # This finally prints the leet word  
+input()   # this is to hold the output on screen in case you are not usind any ide and you are runnign code by just double clicking it.
+# Made by Ravi singh, if you want to suggest anything you can mail on (rvsng028@gmail.com)
+ 
